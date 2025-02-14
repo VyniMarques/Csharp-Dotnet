a89591831bcec9421cbd2ef18328229945a043a4
Vinícius Marques <vinicius.ximenes@ges.inatel.br>
2025-02-14 19:02:44 -0300

    Iniciando repositorio

---
+++ Aula0/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+
+// dotnet new console -n Aula0
+
+// dotnet run
+
+// https://learn.microsoft.com/pt-br/dotnet/csharp/tour-of-csharp/overview
+
+// --> .NET:New Project --> Console App
---
+++ Calculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Calculator;
+
+public class Program
+{
+
+    public static void Main()
+    {
+
+
+        Console.WriteLine("Entre com dois valores");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+        double number2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Qual operação gostaria de realizar?");
+        Console.WriteLine(@"
+        1 - Adição
+        2 - Subtração
+        3 - Multiplicação
+        4 - Divisão
+        5 - Sair
+        ");
+        int op = Convert.ToInt32(Console.ReadLine());
+        double result = 0;
+
+        if (op == 1)
+        {
+            result = number1 + number2;
+
+
+        }
+        else if (op == 2)
+        {
+            result = number1 - number2;
+
+        }
+        else if (op == 3)
+        {
+            result = number1 * number2;
+
+        }
+        else if (op == 4)
+        {
+            result = number1 / number2;
+
+        }
+        else
+        {
+            Console.WriteLine("Fim do Programa");
+        }
+        Console.WriteLine("Resultado =  " + result);
+
+    }
+
+
+}
